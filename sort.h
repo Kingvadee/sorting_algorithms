415abbea47bb6aab661ffd20836d61c339f2ac70
[Kingvadee] <[ojeifodavid1@gmail.com]>
2023-07-24 06:13:57 -0700

    Sort Header File.

--- sort.h
+++ sort.h
@@ -1,6 +1,15 @@
-#ifndef __SORT__
-#define __SORT__
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
 #include <stdlib.h>
+
+
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
@@ -11,39 +20,37 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
-/*======================================================================*/
-/*=================         Printing Algoritms        ==================*/
-/*======================================================================*/
+
+/* aux functions */
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap_bubble(int *a, int *b);
+int partition(int *array, size_t size, int low, int high);
+void partition_sort(int *array, size_t size, int low, int high);
+void merge_sort_call(int *subarray, int *buffer, size_t lo, size_t hi);
+void merge_sub(int *subarray, int *buffer, size_t lo, size_t mid, size_t hi);
+void partition_sort_hoare(int *array, size_t size, int low, int high);
+int partition_hoare(int *array, size_t size, int low, int high);
+void swap_tail(listint_t **list, listint_t **head, listint_t **tail);
+void swap_head(listint_t **list, listint_t **tail, listint_t **head);
 
-
-/*======================================================================*/
-/*=================          sorting Algoritms        ==================*/
-/*======================================================================*/
+/* algorithm functions */
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-int partition(int *array, int lo, int hi, size_t size);
-void quick_s(int *array, int lo, int hi, size_t size);
-void merge(size_t lo, size_t mi, size_t hi, int *dest, int *src);
-void merge_partition(size_t lo, size_t hi, int *array, int *base);
-void swap_root(int *array, size_t root, size_t hi, size_t size);
+void cocktail_sort_list(listint_t **list);
 
-#endif /* __SORT__ */
+
+
+#endif
